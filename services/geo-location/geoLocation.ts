3b3b9b0b729804e688d648b870a73d6415fab3c5
MagdelineJannet <janet.magdeline@gmail.com>
2024-02-22 10:28:45 -0800

    Method to calculate distance and radius between userLocation and Constrution site is included

--- services/geo-location/geoLocation.ts
+++ services/geo-location/geoLocation.ts
@@ -1,18 +1,33 @@
-import ConstructionSite from "../../models/constructionSite.model.js";
+import ConstructionSiteModel from "../../models/constructionSite.model.js";
+import { ILocation } from "../../shared/interfaces/location.interface";
 class GeoLocation {
 
-    async checkInUser(location: Object, siteId: String, workerId: String): Promise<Object> {
+    async checkInUser(userLocation: { latitude: number, longitude: number}, siteId: string, workerId: string): Promise<Object> {
         try {
-            console.log(location,siteId,workerId);
-            const constructionSiteLocation = await this.getLocationOfConstructionSite(siteId);
+            console.log(userLocation,siteId,workerId);
+            const constructionSiteLocation: ILocation | { error: string } = await this.getLocationOfConstructionSite(siteId);
+             
+            if ('error' in constructionSiteLocation) {
+                return { error: constructionSiteLocation.error };
+            }    
           
-            return {"messsage": "check in succesful" };
+            const { coordinates} = constructionSiteLocation;
+             
+            const isWithinRadius = await this.calculateDistanceAndCheckRadius(userLocation.latitude, userLocation.longitude, coordinates[0], coordinates[1], constructionSiteLocation.radius);
+
+             if (isWithinRadius) {
+                 // If the location is within the radius, return a success message
+                 return { message: "check in successful" };
+             } else {
+                 // If the location is outside the radius, return an error message
+                 return { error: "Location is too far from the construction site" };
+             }
           } catch (error) {
             return {"error": error}
           }
     }
 
-    async checkOutUser(location: Object, siteId: String, workerId: String): Promise<Object> {
+    async checkOutUser(location: Object, siteId: string, workerId: string): Promise<Object> {
       try {
           console.log(location,siteId,workerId);
           const constructionSiteLocation = await this.getLocationOfConstructionSite(siteId);
@@ -23,16 +38,32 @@ class GeoLocation {
         }
   }
 
-    async getLocationOfConstructionSite(siteId: String): Promise<Object> {
-        try {
-            const location = await ConstructionSite.find({
-                companyId: siteId
-            });
-            return location;
-          } catch (error) {
-            return {"error": error}
-          }
+    async getLocationOfConstructionSite(siteId: string):  Promise<ILocation | { error: string }> {
+      try {
+        const constructionSite = await ConstructionSiteModel.findOne({ companyId: siteId });
+
+        if (!constructionSite) {
+            return { error: "Construction site not found" };
         }
+
+        const siteLocation = constructionSite.siteLocation as ILocation;
+        return siteLocation;
+    } catch (error) {
+      return { error: "error"};
+    }
+        }
+
+        async calculateDistanceAndCheckRadius(userLatitude: number, userLongitude: number, siteLatitude: number, siteLongitude: number,radius: { type: Number }): Promise<boolean> {
+          const actualRadius = radius.type.valueOf();         
+          const dx = userLatitude - siteLatitude;
+          const dy = userLongitude - siteLongitude;
+          const distanceInKm = Math.sqrt(dx * dx + dy * dy);
+      
+          // Check if the distance is within the specified radius
+          return distanceInKm <= actualRadius;
+      }
+      
+      
     }
 
 
