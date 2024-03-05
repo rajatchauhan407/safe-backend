3274a03d18c7a26f75d4d5fd35d498d974f1c5fb
MagdelineJannet <janet.magdeline@gmail.com>
2024-03-04 21:04:17 -0800

    Changed checkin route to post

--- routes/geoLocationRoutes.ts
+++ routes/geoLocationRoutes.ts
@@ -3,7 +3,7 @@ import Express from "express";
 const router = Express.Router();
 import geoLocationController from '../controllers/geoLocationController.js'
 
-router.get("/checkin",geoLocationController.checkInUser)
+router.post("/checkin",geoLocationController.checkInUser)
 router.get("/checkout",geoLocationController.checkOutUser)
 router.get("/getsafezone",geoLocationController.getSafeZone)
 router.get("/safezonecheck",geoLocationController.checkIfUserInSafeZone)
