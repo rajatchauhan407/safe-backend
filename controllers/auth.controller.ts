000847cccc72b970a13257aea600f8b888a40070
Raha <r.emami98@gmail.com>
2024-03-05 15:00:31 -0800

    SAFE 90 Feature - Login Added into auth Contorller file

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import UserService from '../services/user/userService.js';
+import LoginService from '../services/auth/login.js';
+// ========================================================
 class AuthController {
+  // REGISTER ============================================
   async register(req:Request, res:Response, next:NextFunction) {
     try {
       const userData = req.body;
@@ -10,6 +13,21 @@ class AuthController {
       next(error);
     }
   }
+
+// LOGIN ===============================================
+async login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { userId, password } = req.body;
+    const token = await LoginService.login(userId, password);
+    if (token) {
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
 }
 
 const authController = new AuthController();
