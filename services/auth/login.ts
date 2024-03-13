4f89cd97acf8fa18f0a851597d09211e344203e5
Raha <r.emami98@gmail.com>
2024-03-13 03:01:42 -0700

    SAFE-272-Login Sending the Data of user in frontend

--- services/auth/login.ts
+++ services/auth/login.ts
@@ -1,21 +1,22 @@
 import Authentication from "./auth.js";
 import User from '../../models/user.model.js';
+import ApplicationError from "../../errors/applicationError.js";
+import { IError } from "../../shared/interfaces/error.interface.js";
 // ========================================
 
 class LoginService extends Authentication {
-    static async login(userId: string, password: string): Promise<string | null | boolean> {
+    static async login(userId: string, password: string): Promise<string | null | boolean | IError> {
         try {
           const user = await User.findOne({ userId });
           if (!user) {
-            return 'User not found';
+            throw new ApplicationError('User Not Found',404,'User Not Found','Hey be careful');
           }
     
     // Authenticate user using AuthService methods
       const isAuthenticated = await Authentication.authenticateUser(userId, password);
       if (!isAuthenticated) {
         console.log('Invalid credentials');
-        return false;
-        
+        throw new ApplicationError('Invalid credentials',404,'Invalid Credentials','error details')
       }
 
     // Generate JWT token using AuthService method
@@ -23,7 +24,11 @@ class LoginService extends Authentication {
       return token;
     } catch (error) {
       console.error('Error logging in:', error);
-      return false;
+      if(error instanceof ApplicationError){
+        return error;
+      }
+      return new ApplicationError('Invalid Request',500,'internal server error','details')
+      // return false;
     }
   }
   
